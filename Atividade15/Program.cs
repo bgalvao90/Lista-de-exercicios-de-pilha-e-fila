51dd1a1f57da7d7458970f558a6c0224f008237d
GRUPPY02 <GRUPPY02@DESKTOP-B3K8HS6>
2025-04-15 17:57:07 -0300

    Atividade16

--- Atividade15/Program.cs
+++ Atividade15/Program.cs
@@ -1,2 +1,108 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.CompilerServices;
+
+class Estacionamento
+{
+    private Stack<string> pilhaEstacionamento = new Stack<string>();
+    private const int capacidade = 5;
+
+    public void EntrarCarro(string placa)
+    {
+        if (pilhaEstacionamento.Count >= capacidade)
+        {
+            Console.WriteLine("Estacionamento cheio!");
+            return;
+        }
+        else
+        {
+            Console.Write("Digite a placa do carro: ");
+            placa = Console.ReadLine();
+            pilhaEstacionamento.Push(placa);
+            Console.WriteLine($"Carro placa {placa} estacionou!");
+        }
+    }
+    public void RetirarCarro(string placa)
+    {
+        if (!pilhaEstacionamento.Contains(placa))
+        {
+            Console.WriteLine($"Carro placa {placa} não está no estacionamento!");
+            return;
+        }
+
+        Stack<string> temp = new Stack<string>();
+
+        while (pilhaEstacionamento.Peek() != placa)
+        {
+            string carroremovido = pilhaEstacionamento.Pop();
+            temp.Push(carroremovido);
+            Console.WriteLine($"Carro placa {carroremovido} foi retirado do estacionamento!");
+        }
+        pilhaEstacionamento.Pop();
+        Console.WriteLine($"Carro placa {placa} foi retirado do estacionamento!");
+
+        while(temp.Count > 0)
+        {
+            string carro = temp.Pop();
+            pilhaEstacionamento.Push(carro);
+            Console.WriteLine($"Carro placa {carro} foi recolocado no estacionamento!");
+        }
+    }
+
+    public void MostrarEstacionamento()
+    {
+        Console.WriteLine("O estacionamento contem os seguintes carros.");
+        foreach (var carro in pilhaEstacionamento)
+        {
+            Console.WriteLine(carro);
+        }
+        Console.WriteLine();
+    }
+
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Estacionamento estacionamento = new Estacionamento();
+        string opcao;
+        do
+        {
+            Console.WriteLine();
+            Console.WriteLine("1. Entrar carro");
+            Console.WriteLine("2. Retirar carro");
+            Console.WriteLine("3. Mostrar estacionamento");
+            Console.WriteLine("4. Sair");
+            Console.Write("Escolha uma opção: ");
+            opcao = Console.ReadLine();
+            Console.WriteLine();
+            switch (opcao)
+            {
+                case "1":
+                    Console.Clear();
+                    estacionamento.MostrarEstacionamento();
+                    Console.WriteLine();
+                    estacionamento.EntrarCarro("");
+                    break;
+                case "2":
+                    Console.Clear();
+                    estacionamento.MostrarEstacionamento();
+                    Console.WriteLine();
+                    Console.Write("Digite a placa do carro a ser retirado: ");
+                    string placa = Console.ReadLine();
+                    estacionamento.RetirarCarro(placa);
+                    break;
+                case "3":
+                    Console.Clear();
+                    estacionamento.MostrarEstacionamento();
+                    break;
+                case "4":
+                    Console.WriteLine("Saindo...");
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida.");
+                    break;
+            }
+        } while (opcao != "4");
+    }
+}
---
+++ Atividade16/Program.cs
@@ -0,0 +1,53 @@
+﻿class Deque
+{
+    Queue<int> queue = new Queue<int>();
+    Queue<Queue<int>> queueAux = new Queue<Queue<int>>();
+
+    public void Enqueue(int item)
+    {
+        queue.Enqueue(item);
+        var itemAux = new Queue<int>(queue);
+        queueAux.Enqueue(itemAux);
+    }
+    public void Dequeue()
+    {
+        if (queue.Count == 0 && queueAux.Count == 0)
+            throw new InvalidOperationException("A fila está vazia.");
+        queue.Dequeue();
+        queueAux.Dequeue();
+
+    }
+
+    public void MostrarFila()
+    {
+        Console.WriteLine("Fila atual:");
+        foreach (var item in queue)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine("Fila Secundaria: ");
+        foreach (var item in queueAux)
+        {
+            Console.Write(item + " ");
+            
+        }
+
+
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Deque deque = new Deque();
+        deque.Enqueue(1);
+        deque.Enqueue(2);
+        deque.Enqueue(3);
+        deque.MostrarFila();
+        deque.Dequeue();
+        deque.MostrarFila();
+    }
+}
